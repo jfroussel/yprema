a67685f7606af913fa9557364d07088f29bacc6a
jo <jo@surikat.pro>
2017-09-27 15:25:18 +0200

    bugs fix

--- app/modules/home/tab-home.js
+++ app/modules/home/tab-home.js
@@ -15,6 +15,10 @@ export default class extends Module {
         $.extend(this.data,json);
         var data = this.data;
         data.driver = {};
+        data.statut = '';
+        data.passage = {
+			barcode: '',
+		};
     }
     domReady(){
 
@@ -24,68 +28,43 @@ export default class extends Module {
 
 
         // var element = self.element;
-        let inputBarcode = $("input[name~='barcode']");
+        let inputBarcode = $("input[name=barcode]");
         inputBarcode.focus();
 
-
-
         let barcode = $("#barcode");
-
-        if(barcode.val() == ''){
-            $("#success").hide();
-        }
-
-        if(barcode.val() != null){
-            barcode.focusout(function(){
-                let barcodeVal = barcode.val();
-                if(barcodeVal){
-                
-					$("#success").show();
-					$("#code").append(barcodeVal);
-					$serviceJSON('home/tab-home','getChauffeurInfo',[barcodeVal], function(r){
-						if(!r){
-							r = {};
-						}
-						data.driver = {
-						  "nom":r.nom,
-						  "prenom":r.prenom,
-						  "entreprise":r.entreprise,
-						  "adresse":r.adresse,
-						  "cp":r.code_postal,
-						  "ville":r.ville,
-						  "portable":r.portable,
-						  "email":r.email,
-						  "statut":r.statut,
-						  "solde_base":r.solde_base,
-						  "solde_bonus":r.solde_bonus,
-						  "site_creation":r.site_creation,
-						  "date_creation":r.date_creation,
-					  };
-
-
-					   $("#statut").append("Carte crée le " + moment(r.date_creation).format('DD/MM/YYYY') + " son statut est  " + r.statut );
-
-					   if(r.statut == 'actif'){
-						   $("#success").addClass('alert alert-success');
-					   }else if(r.statut == 'inactif'){
-						   $("#success").addClass('alert alert-danger');
-						   $(".jumbotron").hide();
-						   $("#actualise").show();
-						   $("#actualise").on('click', function(){
-							   location.reload();
-						   });
-					   }
-					});
-				}
-            });
-        }
-
-
-
+		barcode.on('input',function(){
+			let barcodeVal = barcode.val();
+			if(barcodeVal){
+			
+				$serviceJSON('home/tab-home','getChauffeurInfo',[barcodeVal], function(r){
+					
+					let driver = data.driver = r || {};
+					data.statut = driver.id?"Carte crée le " + moment(driver.date_creation).format('DD/MM/YYYY') + " son statut est  " + driver.statut:'';
+					
+					if(driver.id){
+						$("#success").show();
+					}
+					else{
+						$("#success").hide();
+					}
+					
+					if(driver.statut == 'actif'){
+						$("#success").addClass('alert alert-success');
+					}
+					else if(driver.statut == 'inactif'){
+						$("#success").addClass('alert alert-danger');
+						$(".jumbotron").hide();
+						$("#actualise").show();
+						$("#actualise").on('click', function(){
+							location.reload();
+						});
+					}
+				});
+			}
+		});
 
 
         var form = $(this.element).find('form');
-
         form.validate({
             submitHandler: function(e){
                 $serviceJSON('home/tab-home','store',[data.passage],function(r){
@@ -93,7 +72,6 @@ export default class extends Module {
                         $.notify('Le passage a bien été enregistré avec la carte n°' + data.passage.barcode, "success");
                     }
                 });
-                $('#passage').ajax.reload();
             }
         });
 
