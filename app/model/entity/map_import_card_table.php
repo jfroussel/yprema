23b53cd2bf16cc0bafc0d7d5e9b15ecf6c70a315
jo <jo@surikat.pro>
2017-09-27 17:30:49 +0200

    clean

--- app/model/entity/map_import_card_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Model\Entity;
-use App\Model\TableModel;
-
-class Map_Import_Card_Table extends TableModel{
-	protected $uniqColumns = ['user_id'];
-}
--- app/model/entity/map_import_driver_table.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Model\Entity;
-use App\Model\TableModel;
-
-class Map_Import_Driver_Table extends TableModel{
-	protected $uniqColumns = ['user_id'];
-}
